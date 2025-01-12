f9536cd017efa84d789ec523d22a3862ede06104
zhiqiang00 <1941686805@qq.com>
2025-01-12 17:54:51 +0800

    v6_1_1的数据是最新的base,使用master记录,其他的方案新建策略使用新分支合入

--- 金融风险预测赛/utils.py
+++ 金融风险预测赛/utils.py
@@ -1,7 +1,47 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold
+from sklearn.metrics import log_loss, roc_auc_score, mean_squared_error, accuracy_score, f1_score
+import xgboost as xgb
+import lightgbm as lgb
+from lightgbm import log_evaluation, early_stopping
+
+import matplotlib.pyplot as plt
 
 
+# 特征重要性
+def get_feature_importance_pair(gbm_model):
+    feature_importance_pair = [(fe, round(im, 2)) for fe, im in zip(gbm_model.feature_name(), gbm_model.feature_importance())]
+    # 重要性从高到低排序
+    feature_importance_pair = sorted(feature_importance_pair, key=lambda x: x[1], reverse=True)
+    print(feature_importance_pair)
+    feature_name_sorted = [x[0] for x in feature_importance_pair]
+    feature_importance_sorted = [x[1] for x in feature_importance_pair]
+    # for pair in feature_importance_pair:
+    #     print('Feature: {}\t{}'.format(*pair))
+    
+    plt.figure(figsize=(20, 8))
+    # 绘制条形图
+    bars = plt.bar(range(len(feature_name_sorted)), feature_importance_sorted)
+    
+    # 添加数值标签
+    for bar, value in zip(bars, feature_importance_sorted):
+        plt.text(
+            bar.get_x() + bar.get_width() / 2,  # x 坐标，位于条形中心
+            bar.get_height() + 3,  # y 坐标，高度稍微在条形顶部上方
+            str(value),  # 标签内容
+            ha='center',  # 水平居中
+            rotation=45,  # 标签旋转 45 度
+            fontsize=10   # 字体大小
+        )
+
+    plt.xticks(range(len(feature_name_sorted)), feature_name_sorted, rotation=90)
+    plt.xlabel('feature index')
+    plt.ylabel('importance score')
+    plt.show()
+    return feature_importance_pair, feature_name_sorted
+
+# 统计异常数据
 def find_outliers_by_3segama(data, fea):
     data_std = np.std(data[fea])
     data_mean = np.mean(data[fea])
@@ -14,7 +54,7 @@ def find_outliers_by_3segama(data, fea):
     return data
 
 
-def cv_model(clf, train_x, train_y, test_x, clf_name):
+def cv_model(clf, train_x, train_y, test_x, clf_name, is_mms):
     folds = 5
     seed = 2020
     kf = KFold(n_splits=folds, shuffle=True, random_state=seed)
@@ -28,12 +68,12 @@ def cv_model(clf, train_x, train_y, test_x, clf_name):
         print('************************************ {} ************************************'.format(str(i+1)))
         trn_x, trn_y, val_x, val_y = train_x.iloc[train_index], train_y[train_index], train_x.iloc[valid_index], train_y[valid_index]
 
-        print("连续数值进行归一化")
-        mms1 = MinMaxScaler(feature_range=(0, 1))
-        trn_x = mms1.fit_transform(trn_x)
-        val_x = mms1.transform(val_x)
-        mms2 = MinMaxScaler(feature_range=(0, 1))
-        test_x = mms2.fit_transform(test_x)
+        if is_mms == True:
+            print("连续数值进行归一化")
+            mms = MinMaxScaler(feature_range=(0, 1))
+            trn_x = mms.fit_transform(trn_x)
+            val_x = mms.transform(val_x)
+            test_x = mms.fit_transform(test_x)
 
         if clf_name == "lgb":
             train_matrix = clf.Dataset(trn_x, label=trn_y)
@@ -135,17 +175,17 @@ def cv_model(clf, train_x, train_y, test_x, clf_name):
     print("%s_score_std:" % clf_name, np.std(cv_scores))
     return train, test, model
 
-def lgb_model(x_train, y_train, x_test):
-    # https://www.heywhale.com/mw/project/6585325cdcad99bb0a1f4686
-    lgb_train, lgb_test, model = cv_model(lgb, x_train, y_train, x_test, "lgb")
-    return lgb_train, lgb_test, model
+# def lgb_model(x_train, y_train, x_test, is_mms):
+#     # https://www.heywhale.com/mw/project/6585325cdcad99bb0a1f4686
+#     lgb_train, lgb_test, model = cv_model(lgb, x_train, y_train, x_test, "lgb", is_mms)
+#     return lgb_train, lgb_test, model
 
-def xgb_model(x_train, y_train, x_test):
-    # https://www.cnblogs.com/Mephostopheles/p/18397154
-    xgb_train, xgb_test, model = cv_model(xgb, x_train, y_train, x_test, "xgb")
-    return xgb_train, xgb_test, model
+# def xgb_model(x_train, y_train, x_test, is_mms):
+#     # https://www.cnblogs.com/Mephostopheles/p/18397154
+#     xgb_train, xgb_test, model = cv_model(xgb, x_train, y_train, x_test, "xgb", is_mms)
+#     return xgb_train, xgb_test, model
 
-def cat_model(x_train, y_train, x_test):
-    # https://mp.weixin.qq.com/s/xloTLr5NJBgBspMQtxPoFA
-    cat_train, cat_test, model = cv_model(CatBoostRegressor, x_train, y_train, x_test, "cat")
-    return cat_train, cat_test, model
+# def cat_model(x_train, y_train, x_test):
+#     # https://mp.weixin.qq.com/s/xloTLr5NJBgBspMQtxPoFA
+#     cat_train, cat_test, model = cv_model(CatBoostRegressor, x_train, y_train, x_test, "cat")
+#     return cat_train, cat_test, model
